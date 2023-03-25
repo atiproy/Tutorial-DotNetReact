f4ad783966c43da23694311d9f1676651c4b7504
atiproy <atiproy@gmail.com>
2023-03-25 08:50:06 +0530

    Added new interface in typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,32 @@
 import { useEffect, useState } from "react";
+import { Product } from "./product";
 
 function App() {
-  const [products, setProducts] = useState([
-    { name: "p1", price: 1.00 },
-    { name: "p1", price: 11.00 }
-  ])
+  const [products, setProducts] = useState<Product[]>([])
 
-  useEffect(()=>{
-    fetch('https://localhost:7235/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
+  useEffect(() => {
+    fetch('http://localhost:5262/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
   }, [])
 
   function AddProduct() {
-    setProducts(prevstate => [...prevstate, { name: "p" + (prevstate.length + 1), price: (prevstate.length * 11) }])
+    setProducts(prevstate => [...prevstate, {
+      id: prevstate.length + 101,
+      name: 'product' + (prevstate.length + 1),
+      price: (prevstate.length * 11),
+      brand: 'some brand',
+      description: 'some description',
+      pictureUrl: 'http://picsum.photos/200'
+    }])
   }
 
   return (
     <div className="App">
       <h1>Re-store</h1>
       <ul>
-        {products.map((product, index) => (
-          <li key={index}>{product.name} - {product.price}</li>
+        {products.map(product => (
+          <li key={product.id}>{product.name} - {product.price}</li>
         ))}
       </ul>
       <button onClick={AddProduct}>Add Product</button>

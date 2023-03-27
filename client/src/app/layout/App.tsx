5fa625fc4635d6abad08ddbfbded878b4030ec63
atiproy <atiproy@gmail.com>
2023-03-27 18:31:12 +0530

    Dark mode introduced, remaining the switch

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,34 +1,27 @@
-import { Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { light } from "@mui/material/styles/createPalette";
+import { Container } from "@mui/system";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
+import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([])
-
-  useEffect(() => {
-    fetch('http://localhost:5262/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-  function AddProduct() {
-    setProducts(prevstate => [...prevstate, {
-      id: prevstate.length + 101,
-      name: 'product' + (prevstate.length + 1),
-      price: (prevstate.length * 11),
-      brand: 'some brand',
-      description: 'some description',
-      pictureUrl: 'http://picsum.photos/200'
-    }])
-  }
+  const [darkMode, setDarkMode]= useState(false);
+  const palatteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: palatteType
+    }
+  })
 
   return (
-    <>
-      <Typography variant="h1">Re-store</Typography>
-      <Catalog products={products} addProduct={AddProduct} />
-
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
   );
 }
 

d016ae3fffb2aa3c7c398ce1959032a7ae19c953
atiproy <atiproy@gmail.com>
2023-03-28 18:04:53 +0530

    updated navLinks

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,31 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+    { title: 'catalog', path: '/catalog' },
+    { title: 'about', path: '/about' },
+    { title: 'contact', path: '/contact' },
+]
+
+const rightLinks = [
+    { title: 'login', path: '/login' },
+    { title: 'register', path: '/register' },
+]
+
+const navStyles = {
+    color: 'inherit',
+    typography: 'h6',
+    fontWeight: "400",
+    textDecoration: "none",
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
 
 interface Props {
     darkmode: boolean;
@@ -8,11 +35,52 @@ interface Props {
 export default function Header({ darkmode, handleThemeChange }: Props) {
     return (
         <AppBar position="static" sx={{ mb: 4 }}>
-            <Toolbar>
-                <Typography variant="h6">
-                    RE-STORE
-                </Typography>
-                <Switch checked={darkmode} onChange={handleThemeChange} />
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+
+                <Box>
+                    <Typography
+                        variant="h6"
+                        component={NavLink}
+                        to="/"
+                        sx={navStyles}
+                    >
+                        RE-STORE
+                    </Typography>
+                    <Switch checked={darkmode} onChange={handleThemeChange} />
+                </Box>
+                <Box>
+                    <List sx={{ display: 'flex' }}>
+                        {midLinks.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                <Box display='flex' alignItems='center'>
+                    <IconButton sx={navStyles}>
+                        <Badge badgeContent='4' color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
             </Toolbar>
         </AppBar>
     )

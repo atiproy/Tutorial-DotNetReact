cb5eb620972bff3c743ebfa2c73a03d9e7835696
atiproy <atiproy@gmail.com>
2023-07-21 17:01:23 +0530

    completed subtotal

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,10 +1,11 @@
-import { Box, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Box, Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { Add, Delete, Remove } from "@mui/icons-material";
 import { useStoreContext } from "../../app/Context/StoreContext";
 import { useState } from "react";
 import agent from "../../app/api/agent";
 import { error } from "console";
 import { LoadingButton } from "@mui/lab";
+import BasketSummary from "./BasketSummary";
 
 export default function BasketPage() {
 
@@ -34,60 +35,68 @@ export default function BasketPage() {
 
 
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Product</TableCell>
-                        <TableCell align="right">Price</TableCell>
-                        <TableCell align="center">Quantity</TableCell>
-                        <TableCell align="right">Subtotal</TableCell>
-                        <TableCell align="right"></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {basket.items.map(item => (
-                        <TableRow
-                            key={item.productId}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                <Box display='flex' alignItems='center'>
-                                    <img src={item.pictureUrl} alt={item.name} style={{ height: 50, marginRight: 20 }} />
-                                    <span>{item.name}</span>
-                                </Box>
-                            </TableCell>
-                            <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
-                            <TableCell align="center">
-                                <LoadingButton
-                                    loading={status.loading && status.name === 'rem' + item.productId}
-                                    onClick={() => handleRemoveItem(item.productId, 1, 'rem' + item.productId)}
-                                    color="error"
-                                >
-                                    <Remove />
-                                </LoadingButton>
-                                {item.quanity}
-                                <LoadingButton
-                                    loading={status.loading && status.name === 'add' + item.productId}
-                                    onClick={() => handleAddItem(item.productId, 'add' + item.productId)}
-                                    color="error"
-                                >
-                                    <Add />
-                                </LoadingButton>
-                            </TableCell>
-                            <TableCell align="right">${(item.price * item.quanity / 100).toFixed(2)}</TableCell>
-                            <TableCell align="right">
-                                <LoadingButton
-                                    loading={status.loading && status.name === 'del' + item.productId}
-                                    onClick={() => handleRemoveItem(item.productId, item.quanity, 'del' + item.productId)}
-                                    color="error"
-                                ><Delete />
-                                </LoadingButton>
-                            </TableCell>
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Product</TableCell>
+                            <TableCell align="right">Price</TableCell>
+                            <TableCell align="center">Quantity</TableCell>
+                            <TableCell align="right">Subtotal</TableCell>
+                            <TableCell align="right"></TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {basket.items.map(item => (
+                            <TableRow
+                                key={item.productId}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    <Box display='flex' alignItems='center'>
+                                        <img src={item.pictureUrl} alt={item.name} style={{ height: 50, marginRight: 20 }} />
+                                        <span>{item.name}</span>
+                                    </Box>
+                                </TableCell>
+                                <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
+                                <TableCell align="center">
+                                    <LoadingButton
+                                        loading={status.loading && status.name === 'rem' + item.productId}
+                                        onClick={() => handleRemoveItem(item.productId, 1, 'rem' + item.productId)}
+                                        color="error"
+                                    >
+                                        <Remove />
+                                    </LoadingButton>
+                                    {item.quanity}
+                                    <LoadingButton
+                                        loading={status.loading && status.name === 'add' + item.productId}
+                                        onClick={() => handleAddItem(item.productId, 'add' + item.productId)}
+                                        color="error"
+                                    >
+                                        <Add />
+                                    </LoadingButton>
+                                </TableCell>
+                                <TableCell align="right">${(item.price * item.quanity / 100).toFixed(2)}</TableCell>
+                                <TableCell align="right">
+                                    <LoadingButton
+                                        loading={status.loading && status.name === 'del' + item.productId}
+                                        onClick={() => handleRemoveItem(item.productId, item.quanity, 'del' + item.productId)}
+                                        color="error"
+                                    ><Delete />
+                                    </LoadingButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Grid container>
+                <Grid item xs={6} />
+                <Grid item xs={6}>
+                    <BasketSummary />
+                </Grid>
+            </Grid>
+        </>
     )
 }
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -6,6 +6,7 @@ import agent from "../../app/api/agent";
 import { error } from "console";
 import { LoadingButton } from "@mui/lab";
 import { useStoreContext } from "../../app/Context/StoreContext";
+import { currencyFormat } from "../../app/Util/util";
 
 interface Props {
     product: Product
@@ -48,7 +49,7 @@ export default function ProductCard({ product }: Props) {
                 />
                 <CardContent>
                     <Typography gutterBottom color='secondary' variant="h5" >
-                        ₹{(product.price / 100).toFixed(2)}
+                       {currencyFormat(product.price)}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                         {product.brand} / {product.type}
